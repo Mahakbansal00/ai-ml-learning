54907863a74138edef80b228066e82a6bb290d5c
Mahak Bansal <mahakbansal82@gmail.com>
2026-07-18 17:59:37 +0530

    Day 1: Python Basics

---
+++ Day-01-Python-Basics/datatypes.py
@@ -0,0 +1,18 @@
+"""INT STRING FLOAT BOOLEAN NONE Data type"""
+a = 10
+b= "babita"
+c = 2.2
+d = True
+e = None
+"""type of operator arthmmetic operator relational operator logical operator assignment operator"""
+f=a+c
+print(f)#arthmetic operator
+if(a>c):
+  print("a is greater than b")#relational operator
+age = 24
+if(24>18 and 24<30):
+  print("you are eligible")#logical operator
+a=20
+a=a+10
+a+=10
+print(a)#assignment operator
