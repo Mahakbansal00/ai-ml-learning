942e15f3a1fbbf4d7b7fe60460eb324849f65ebf
Mahak Bansal <mahakbansal82@gmail.com>
2026-07-20 13:08:34 +0530

    Day 2: Operators and Conditional Statements

---
+++ Strings & Conditional Statements/Conditional statements.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number: "))
+if(a%2==0):
+ #4space   
+   print("even")
+elif(a%2!=0):
+     print("odd")
+else:
+    print("not a number")
---
+++ input_output.py
@@ -0,0 +1,37 @@
+"""Strings are a data type that stores a sequence of characters, and can be created in different ways
+Python uses double quotes and escape sequences for creating strings.
+
+Concatenating and finding length of strings in Python
+
+String manipulation in Python and understanding string length and indexing
+
+Slicing in Python involves accessing parts of a string and is crucial for dealing with data in machine learning.
+
+Explaining string indexing and slicing in Python
+
+Backward counting is helpful for string manipulation
+
+Understanding string functions, ends with
+
+Learn about string replacement and find functions in Python
+
+Count function in strings can be used to find the occurrences of a substring
+
+Conditional statements are used to execute code based on specific conditions.
+
+Using indentation and conditional statements in Python
+
+Difference between 'if' and 'lf'
+
+Using 'if' and 'else' statements in Python
+
+Using conditional statements and logical operators to calculate grades.
+
+Using conditional statements to determine grades based on student marks in Python
+
+Even numbers are divisible by two with no remainder
+
+Program to find the greatest value among three numbers
+
+Understanding multiples and basic coding practice
+"""
